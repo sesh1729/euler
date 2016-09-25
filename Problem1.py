44c39641a45e66c0d8e045d931c6dc771e73c9e4
Seshadri <seshadri.kolluri@gmail.com>
2016-09-25 15:25:46 -0700

    fixed Problem 1

--- Problem1.py
+++ Problem1.py
@@ -5,5 +5,5 @@ for number in range (1,1000):
     if (number % 3 ==0 or number%5 == 0):
         #print number
         sum1 = sum1 + number
-print "the sum1 is %s"%sum
+print "the sum1 is %s"%sum1
 
